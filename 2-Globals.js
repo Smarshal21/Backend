6242597e3a84482b3f14416db36145c2f81ae410
Smarshall <lcb2021048@iiitl.ac.in>
2023-08-26 16:26:11 +0530

    Before Adding the Modules

---
+++ 2-Globals.js
@@ -0,0 +1,10 @@
+
+//__dirname
+//__filename
+//require
+//module
+//process
+console.log(__dirname);
+setInterval(()=>{
+    console.log('hello world');
+},1000);
---
+++ 3-Modules.js
@@ -0,0 +1,10 @@
+// Modules
+const sam = 'sam';
+const man = 'man';
+const Sayhi = (name) => {
+    console.log(`hello ${name}`);
+};
+
+Sayhi('Sammy');
+Sayhi(sam);
+Sayhi(man);
